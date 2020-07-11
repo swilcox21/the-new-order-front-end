7278b7b7fa5a887e6f7df26633ad2a22bfaa0f4e
Niza Vera <nizavera95@gmail.com>
2020-07-11 01:38:31 +0000

    Adding components to login file

--- src/js/views/login.js
+++ src/js/views/login.js
@@ -1,4 +1,86 @@
 import React from "react";
 import "../../styles/home.scss";
 
-export const Login = () => <h1>Hello</h1>;
+export const Login = () => {
+	return (
+		<div className="container text-center">
+			<div className="row">
+				<input
+					type="email"
+					className="form-control"
+					id="exampleInputEmail1"
+					aria-describedby="emailHelp"
+					placeholder="Enter email"
+				/>
+			</div>
+			<div className="row mt-2">
+				<div className="col-sm border-white p-0">
+					<button className="btn btn-primary w-100" type="submit">
+						{"1"}
+					</button>
+				</div>
+				<div className="col-sm">
+					<button className="btn btn-primary w-100" type="submit">
+						{"2"}
+					</button>
+				</div>
+				<div className="col-sm">
+					<button className="btn btn-primary w-100" type="submit">
+						{"3"}
+					</button>
+				</div>
+			</div>
+			<div className="row">
+				<div className="col-sm">
+					<button className="btn btn-primary w-100" type="submit">
+						{"4"}
+					</button>
+				</div>
+				<div className="col-sm">
+					<button className="btn btn-primary w-100" type="submit">
+						{"5"}
+					</button>
+				</div>
+				<div className="col-sm">
+					<button className="btn btn-primary w-100" type="submit">
+						{"6"}
+					</button>
+				</div>
+			</div>
+			<div className="row">
+				<div className="col-sm">
+					<button className="btn btn-primary w-100" type="submit">
+						{"7"}
+					</button>
+				</div>
+				<div className="col-sm">
+					<button className="btn btn-primary w-100" type="submit">
+						{"8"}
+					</button>
+				</div>
+				<div className="col-sm">
+					<button className="btn btn-primary w-100" type="submit">
+						{"9"}
+					</button>
+				</div>
+			</div>
+			<div className="row">
+				<div className="col-sm">
+					<button className="btn btn-primary w-100" type="submit">
+						{"Clear"}
+					</button>
+				</div>
+				<div className="col-sm">
+					<button className="btn btn-primary w-100" type="submit">
+						{"0"}
+					</button>
+				</div>
+				<div className="col-sm">
+					<button className="btn btn-primary w-100" type="submit">
+						{"Enter"}
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
