2cb5144aa81b055c7723aa41fcf44eb21bfc912e
codekrak <codekrak68@gmail.com>
2020-08-20 03:05:30 +0000

    drag and drop javascript for admin main menu

--- src/js/component/adminMainMenu.js
+++ src/js/component/adminMainMenu.js
@@ -3,6 +3,82 @@ import "../../styles/adminMainMenu.scss";
 import { Link } from "react-router-dom";
 
 export const AdminMainMenu = () => {
+	function handleDragStart(e) {
+		this.style.opacity = "0.4";
+	}
+
+	function handleDragEnd(e) {
+		this.style.opacity = "1";
+	}
+
+	let items = document.querySelectorAll(".container .box");
+	items.forEach(function(item) {
+		item.addEventListener("dragstart", handleDragStart, false);
+		item.addEventListener("dragend", handleDragEnd, false);
+	});
+
+	document.addEventListener("DOMContentLoaded", event => {
+		function handleDragStart(e) {
+			this.style.opacity = "0.4";
+		}
+
+		function handleDragEnd(e) {
+			this.style.opacity = "1";
+
+			items.forEach(function(item) {
+				item.classList.remove("over");
+			});
+		}
+
+		function handleDragOver(e) {
+			if (e.preventDefault) {
+				e.preventDefault();
+			}
+
+			return false;
+		}
+
+		function handleDragEnter(e) {
+			this.classList.add("over");
+		}
+
+		function handleDragLeave(e) {
+			this.classList.remove("over");
+		}
+
+		let items = document.querySelectorAll(".container .box");
+		items.forEach(function(item) {
+			item.addEventListener("dragstart", handleDragStart, false);
+			item.addEventListener("dragover", handleDragOver, false);
+			item.addEventListener("dragenter", handleDragEnter, false);
+			item.addEventListener("dragleave", handleDragLeave, false);
+			item.addEventListener("dragend", handleDragEnd, false);
+		});
+	});
+	function handleDrop(e) {
+		e.stopPropagation(); // stops the browser from redirecting.
+		return false;
+	}
+
+	function handleDragStart(e) {
+		this.style.opacity = "0.4";
+
+		dragSrcEl = this;
+
+		e.dataTransfer.effectAllowed = "move";
+		e.dataTransfer.setData("text/html", this.innerHTML);
+	}
+
+	function handleDrop(e) {
+		e.stopPropagation();
+
+		if (dragSrcEl !== this) {
+			dragSrcEl.innerHTML = this.innerHTML;
+			this.innerHTML = e.dataTransfer.getData("text/html");
+		}
+
+		return false;
+	}
 	return (
 		<div className="container-fluid">
 			<div className="jumbotron3 image mb-3">
@@ -23,10 +99,20 @@ export const AdminMainMenu = () => {
 			</div>
 			<div className="body">
 				{/*DRAG AND DROP START*/}
-
+				<div className="dragdrop">
+					<div draggable="true" className="box">
+						A
+					</div>
+					<div draggable="true" className="box">
+						B
+					</div>
+					<div draggable="true" className="box">
+						C
+					</div>
+				</div>
 				{/*DRAG AND DROP END*/}
 
-				<div className="menu">
+				{/*<div className="menu">
 					<table className="menutab">
 						<tr>
 							<td>
@@ -54,7 +140,7 @@ export const AdminMainMenu = () => {
 							</td>
 						</tr>
 					</table>
-				</div>
+				</div>*/}
 			</div>
 		</div>
 	);
