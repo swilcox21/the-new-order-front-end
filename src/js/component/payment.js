b6d806465733a6f0460614a885ad644c3ad4a917
Niza Vera <nizavera95@gmail.com>
2020-08-27 14:00:31 +0000

    wrapped up design portion of payment page

--- src/js/component/payment.js
+++ src/js/component/payment.js
@@ -57,51 +57,22 @@ export const Payment = () => {
 							</div>
 							<span className="text-muted">$5</span>
 						</li>
-						<li className="list-group-item d-flex justify-content-between bg-light">
-							<div className="text-success">
-								<h6 className="my-0">Promo code</h6>
-								<small>EXAMPLECODE</small>
-							</div>
-							<span className="text-success">-$5</span>
-						</li>
 						<li className="list-group-item d-flex justify-content-between">
 							<span>Total (USD)</span>
 							<strong>$20</strong>
 						</li>
 					</ul>
-
-					<form className="card p-2">
-						<div className="input-group">
-							<input type="text" className="form-control" placeholder="Promo code" />
-							<div className="input-group-append">
-								<button type="button" className="btn btn-secondary">
-									Redeem
-								</button>
-							</div>
-						</div>
-					</form>
 				</div>
 				<div className="col-md-8 order-md-1">
-					<h4 className="mb-3">Billing address</h4>
+					<h4 className="mb-3">{"Your Contact Information"}</h4>
 					<form className="needs-validation" noValidate>
 						<div className="row">
-							<div className="col-md-6 mb-3">
-								<label htmlFor="firstName">First name</label>
+							<div className="col-md-12 mb-3">
+								<label htmlFor="Name">{"Name"}</label>
 								<input
 									type="text"
 									className="form-control"
-									id="firstName"
-									placeholder=""
-									value=""
-									required
-								/>
-							</div>
-							<div className="col-md-6 mb-3">
-								<label htmlFor="lastName">Last name</label>
-								<input
-									type="text"
-									className="form-control"
-									id="lastName"
+									id="Name"
 									placeholder=""
 									value=""
 									required
@@ -110,88 +81,15 @@ export const Payment = () => {
 						</div>
 
 						<div className="mb-3">
-							<label htmlFor="username">Username</label>
-							<div className="input-group">
-								<div className="input-group-prepend">
-									<span className="input-group-text">@</span>
-								</div>
-								<input
-									type="text"
-									className="form-control"
-									id="username"
-									placeholder="Username"
-									required
-								/>
-							</div>
-						</div>
-
-						<div className="mb-3">
-							<label htmlFor="email">
-								Email <span className="text-muted">(Optional)</span>
-							</label>
+							<label htmlFor="email">{"Email"}</label>
 							<input type="email" className="form-control" id="email" placeholder="you@example.com" />
 						</div>
 
 						<div className="mb-3">
-							<label htmlFor="address">Address</label>
-							<input
-								type="text"
-								className="form-control"
-								id="address"
-								placeholder="1234 Main St"
-								required
-							/>
-							<div className="invalid-feedback">Please enter your shipping address.</div>
+							<label htmlFor="phone">{"Phone"}</label>
+							<input type="text" className="form-control" id="phone" placeholder="9546783325" required />
 						</div>
 
-						<div className="mb-3">
-							<label htmlFor="address2">
-								Address 2 <span className="text-muted">(Optional)</span>
-							</label>
-							<input
-								type="text"
-								className="form-control"
-								id="address2"
-								placeholder="Apartment or suite"
-							/>
-						</div>
-
-						<div className="row">
-							<div className="col-md-5 mb-3">
-								<label htmlFor="country">Country</label>
-								<select className="custom-select d-block w-100" id="country" required>
-									<option value="">Choose...</option>
-									<option>United States</option>
-								</select>
-								<div className="invalid-feedback">Please select a valid country.</div>
-							</div>
-							<div className="col-md-4 mb-3">
-								<label htmlFor="state">State</label>
-								<select className="custom-select d-block w-100" id="state" required>
-									<option value="">Choose...</option>
-									<option>California</option>
-								</select>
-								<div className="invalid-feedback">Please provide a valid state.</div>
-							</div>
-							<div className="col-md-3 mb-3">
-								<label htmlFor="zip">Zip</label>
-								<input type="text" className="form-control" id="zip" placeholder="" required />
-								<div className="invalid-feedback">Zip code required.</div>
-							</div>
-						</div>
-						<hr className="mb-4" />
-						<div className="custom-control custom-checkbox">
-							<input type="checkbox" className="custom-control-input" id="same-address" />
-							<label className="custom-control-label" htmlFor="same-address">
-								Shipping address is the same as my billing address
-							</label>
-						</div>
-						<div className="custom-control custom-checkbox">
-							<input type="checkbox" className="custom-control-input" id="save-info" />
-							<label className="custom-control-label" htmlFor="save-info">
-								Save this information for next time
-							</label>
-						</div>
 						<hr className="mb-4" />
 
 						<h4 className="mb-3">Payment</h4>
@@ -230,7 +128,7 @@ export const Payment = () => {
 									className="custom-control-input"
 									required
 								/>
-								<label className="custom-control-label" htmlFor="paypal">
+								<label className="custom-control-label" htmlFor="https://www.paypal.com/paypalme/">
 									PayPal
 								</label>
 							</div>
