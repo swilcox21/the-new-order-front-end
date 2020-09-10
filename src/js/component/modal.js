894e38a9414303eba04907e4406bd276420160e4
marie721 <mdarvola@gmail.com>
2020-09-10 17:41:18 +0000

    changes to navbar.js and modal.js

--- src/js/component/modal.js
+++ src/js/component/modal.js
@@ -1,76 +1,163 @@
+
+
 import React, { useState, useEffect, useContext } from "react";
-import ReactDOM, { Link } from "react-dom";
+
+import { Link, useHistory } from "react-router-dom";
+
 import PropTypes from "prop-types";
+
 import { useForm } from "react-hook-form";
+
 import { Context } from "../store/appContext";
 
+​
+
 export const Modal = props => {
-	const [state, setState] = useState({});
+
 	const { login, handleSubmit } = useForm();
+
 	const { store, actions } = useContext(Context);
+
 	const [email, setEmail] = useState("");
+
 	const [password, setPassword] = useState("");
 
+	const history = useHistory();
+
+​
+
 	return (
+
 		<div
+
 			className="modal fade"
-			id="exampleModal"
+
+			id={props.id}
+
+			style={{ display: props.show ? "inline-block" : "none" }}
+
 			tabIndex="-1"
+
 			aria-labelledby="exampleModalLabel"
+
 			aria-hidden="true">
+
 			<div className="modal-dialog">
+
 				<div className="modal-content">
+
 					<div className="modal-header">
+
 						<h5 className="modal-title" id="exampleModalLabel">
+
 							Vendor Login
+
 						</h5>
+
 						<button type="button" className="close" data-dismiss="modal" aria-label="Close">
+
 							<span aria-hidden="true">&times;</span>
-						</button>
-					</div>
-					<div className="modal-body">
-						<div className="form-group">
-							<label htmlFor="exampleInputEmail1">Email address</label>
-							<input
-								type="email"
-								className="form-control"
-								id="exampleInputEmail1"
-								aria-describedby="emailHelp"
-								onChange={e => setEmail(e.target.value)}
-							/>
-						</div>
-						<div className="form-group">
-							<label htmlFor="exampleInputPassword1">Password</label>
-							<input
-								type="password"
-								className="form-control"
-								id="exampleInputPassword1"
-								onChange={e => setPassword(e.target.value)}
-							/>
-						</div>
-					</div>
-					{/* {store.token != null ? ( */}
-					<div className="modal-footer">
-						<button type="button" className="btn btn-secondary" data-dismiss="modal">
-							Close
+
 						</button>
 
-						{/* <Link to="/admin-main-menu"> */}
-						<button
-							type="button"
-							onClick={() => actions.login(email, password)}
-							className="btn btn-primary">
-							Login
-						</button>
-						{/* </Link> */}
 					</div>
-					{/* ) : null} */}
+
+					<div className="modal-body">
+
+						<div className="form-group">
+
+							<label htmlFor="exampleInputEmail1">Email address</label>
+
+							<input
+
+								type="email"
+
+								className="form-control"
+
+								id="exampleInputEmail1"
+
+								aria-describedby="emailHelp"
+
+								onChange={e => setEmail(e.target.value)}
+
+							/>
+
+						</div>
+
+						<div className="form-group">
+
+							<label htmlFor="exampleInputPassword1">Password</label>
+
+							<input
+
+								type="password"
+
+								className="form-control"
+
+								id="exampleInputPassword1"
+
+								onChange={e => setPassword(e.target.value)}
+
+							/>
+
+						</div>
+
+					</div>
+
+​
+
+					<div className="modal-footer">
+
+						<button type="button" className="btn btn-secondary" data-dismiss="modal">
+
+							Close
+
+						</button>
+
+​
+
+						<button
+
+							data-dismiss="modal"
+
+							type="button"
+
+							onClick={e => {
+
+								actions.login(email, password);
+
+								history.push("/admin-main-menu");
+
+							}}
+
+							className="btn btn-primary">
+
+							Login
+
+						</button>
+
+					</div>
+
 				</div>
+
 			</div>
+
 		</div>
+
 	);
+
 };
+
+​
 
 Modal.propTypes = {
-	onClose: PropTypes.func
+
+	onClose: PropTypes.func,
+
+	id: PropTypes.string,
+
+	show: PropTypes.bool
+
 };
+
+
--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,95 +5,208 @@ import { Modal } from "./modal";
 import "../../styles/navbar.scss";
 import { Context } from "../store/appContext";
 
+​
 export const Navbar = props => {
+
 	const { store, actions } = useContext(Context);
-	const [state, setState] = useState({});
+
+​
+
+	const [state, setState] = useState({
+
+		showModal: false
+
+	});
+
 	const onSubmit = data => {};
+
 	return (
+
 		<>
+
 			<nav className="navbar navbar-expand-lg ">
-				<a className="navbar-brand text-white" href="#">
+
+				<a className="navbar-brand text-danger" href="#">
+
 					The New Order
+
 				</a>
+
 				<button
+
 					className="navbar-toggler"
+
 					type="button"
+
 					data-toggle="collapse"
+
 					data-target="#navbarNavDropdown"
+
 					aria-controls="navbarNavDropdown"
+
 					aria-expanded="false"
+
 					aria-label="Toggle navigation">
+
 					<span className="navbar-toggler-icon" />
+
 				</button>
-				<div className="collapse navbar-collapse" id="navbarNavDropdown">
+
+				<div className="collapse navbar-collapse " id="navbarNavDropdown">
+
 					<ul className="navbar-nav ">
+
 						{/* {props.test === "home" ? null : (
+
 							<li className="nav-item active">
+
 								<a className="nav-link" href="#">
+
 									Home <span className="sr-only">(current)</span>
+
 								</a>
+
 							</li>
+
 						)} */}
+
 						{store.token != null ? (
+
 							<li className="nav-item ">
+
 								<Link className="nav-link text-white" to="/admin-main-menu">
+
 									Admin Main Menu
+
 								</Link>
+
 							</li>
+
 						) : null}
+
 						<li className="nav-item ">
+
 							<Link className="nav-link text-white" to="/maps">
+
 								Find a Vendor
+
 							</Link>
+
 						</li>
+
+​
 
 						<li className="nav-item dropdown">
+
 							<a
+
 								className="nav-link dropdown-toggle text-white"
+
 								href="#"
+
 								id="navbarDropdownMenuLink"
+
 								role="button"
+
 								data-toggle="dropdown"
+
 								aria-haspopup="true"
+
 								aria-expanded="false">
+
 								Vendors
+
 							</a>
 
+​
+
 							<div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-								<div
-									type="button"
-									className="dropdown-item"
-									data-toggle="modal"
-									data-target="#exampleModal">
-									Login
-								</div>
+
+								{store.token != null ? (
+
+									<div onClick={() => actions.logout()} type="button" className="dropdown-item">
+
+										Logout
+
+									</div>
+
+								) : (
+
+									<div
+
+										onClick={() => setState({ showModal: true })}
+
+										type="button"
+
+										className="dropdown-item"
+
+										data-toggle="modal"
+
+										data-target="#exampleModal">
+
+										Login
+
+									</div>
+
+								)}
+
 								{/* </Link> */}
+
 								{/* ) : null} */}
+
 								<Link className="dropdown-item" to="/signUp">
+
 									Register your business
+
 								</Link>
+
 							</div>
+
 						</li>
+
 						{/* <form className="form-inline my-2 my-lg-0">
+
 							<input
+
 								className="form-control mr-sm-2"
+
 								type="search"
+
 								placeholder="Find a Vendor"
+
 								aria-label="Search"
+
 							/>
+
 							<button className="btn btn-success my-2 my-sm-0" type="submit">
+
 								Search
+
 							</button>
+
 						</form> */}
+
 					</ul>
+
 				</div>
+
 			</nav>
 
-			<Modal />
+​
+
+			<Modal id="exampleModal" show={state.showModal} onClose={() => setState({ showModal: false })} />
+
 		</>
+
 	);
+
 };
+
 Navbar.propTypes = {
+
 	test: PropTypes.string,
+
 	onSubmit: PropTypes.func
+
 };
+
