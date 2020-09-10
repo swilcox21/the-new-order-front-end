4ff469e431b8ef076e2508ed5f9ecbd50cd5f2ae
codekrak <codekrak68@gmail.com>
2020-09-10 17:39:22 +0000

    worked on styling and modal navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -4,15 +4,16 @@ import PropTypes from "prop-types";
 import { Modal } from "./modal";
 import "../../styles/navbar.scss";
 import { Context } from "../store/appContext";
-
 export const Navbar = props => {
 	const { store, actions } = useContext(Context);
-	const [state, setState] = useState({});
+	const [state, setState] = useState({
+		showModal: false
+	});
 	const onSubmit = data => {};
 	return (
 		<>
 			<nav className="navbar navbar-expand-lg ">
-				<a className="navbar-brand text-white" href="#">
+				<a className="navbar-brand text-danger" href="#">
 					The New Order
 				</a>
 				<button
@@ -25,15 +26,15 @@ export const Navbar = props => {
 					aria-label="Toggle navigation">
 					<span className="navbar-toggler-icon" />
 				</button>
-				<div className="collapse navbar-collapse" id="navbarNavDropdown">
+				<div className="collapse navbar-collapse " id="navbarNavDropdown">
 					<ul className="navbar-nav ">
 						{/* {props.test === "home" ? null : (
-							<li className="nav-item active">
-								<a className="nav-link" href="#">
-									Home <span className="sr-only">(current)</span>
-								</a>
-							</li>
-						)} */}
+                            <li className="nav-item active">
+                                <a className="nav-link" href="#">
+                                    Home <span className="sr-only">(current)</span>
+                                </a>
+                            </li>
+                        )} */}
 						{store.token != null ? (
 							<li className="nav-item ">
 								<Link className="nav-link text-white" to="/admin-main-menu">
@@ -46,7 +47,6 @@ export const Navbar = props => {
 								Find a Vendor
 							</Link>
 						</li>
-
 						<li className="nav-item dropdown">
 							<a
 								className="nav-link dropdown-toggle text-white"
@@ -58,15 +58,21 @@ export const Navbar = props => {
 								aria-expanded="false">
 								Vendors
 							</a>
-
 							<div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-								<div
-									type="button"
-									className="dropdown-item"
-									data-toggle="modal"
-									data-target="#exampleModal">
-									Login
-								</div>
+								{store.token != null ? (
+									<div onClick={() => actions.logout()} type="button" className="dropdown-item">
+										Logout
+									</div>
+								) : (
+									<div
+										onClick={() => setState({ showModal: true })}
+										type="button"
+										className="dropdown-item"
+										data-toggle="modal"
+										data-target="#exampleModal">
+										Login
+									</div>
+								)}
 								{/* </Link> */}
 								{/* ) : null} */}
 								<Link className="dropdown-item" to="/signUp">
@@ -75,21 +81,20 @@ export const Navbar = props => {
 							</div>
 						</li>
 						{/* <form className="form-inline my-2 my-lg-0">
-							<input
-								className="form-control mr-sm-2"
-								type="search"
-								placeholder="Find a Vendor"
-								aria-label="Search"
-							/>
-							<button className="btn btn-success my-2 my-sm-0" type="submit">
-								Search
-							</button>
-						</form> */}
+                            <input
+                                className="form-control mr-sm-2"
+                                type="search"
+                                placeholder="Find a Vendor"
+                                aria-label="Search"
+                            />
+                            <button className="btn btn-success my-2 my-sm-0" type="submit">
+                                Search
+                            </button>
+                        </form> */}
 					</ul>
 				</div>
 			</nav>
-
-			<Modal />
+			<Modal id="exampleModal" show={state.showModal} onClose={() => setState({ showModal: false })} />
 		</>
 	);
 };
