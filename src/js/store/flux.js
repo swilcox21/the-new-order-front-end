99fa37382cece3edda539c898518dd2f74bb6d0b
Niza Vera <nizavera95@gmail.com>
2020-09-11 21:26:30 +0000

    wrong url

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,4 +1,4 @@
-const backendApiUrl = "https://3000-a9e77093-c97a-44b3-b9fd-a511d32e002c.ws-us02.gitpod.io/git ";
+const backendApiUrl = "https://3000-a9e77093-c97a-44b3-b9fd-a511d32e002c.ws-us02.gitpod.io/";
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
